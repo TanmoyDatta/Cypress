1904b63f222fc686506a93800700a0a18a06ff1c
Tanmoy Datta <tonmoydatta45@gmail.com>
2023-03-22 17:43:18 +0600

    second cypress comiit

--- cypress/e2e/Basic/FirstTest.cy.js
+++ cypress/e2e/Basic/FirstTest.cy.js
@@ -1,10 +1,6 @@
 describe('First Test', () => {
   it('Open Url', () => {
     cy.visit('https://www.guru99.com/');
-    console.log(cy.title().text);
-    cy.title().should(
-      'eq',
-      'Meet Guru99 – Free Training Tutorials & Video for IT Courses'
-    );
+    cy.title().should('eq','Meet Guru99 – Free Training Tutorials & Video for IT Courses');
   });
 });
---
+++ cypress/e2e/Basic/Locators.cy.js
@@ -0,0 +1,14 @@
+describe('Locator Test', () => {
+  it('Open Url', () => {
+    cy.visit('https://www.guru99.com/');
+    cy.title().should(
+      'eq',
+      'Meet Guru99 – Free Training Tutorials & Video for IT Courses'
+    );
+  });
+  it('select option', () => {
+    cy.visit('https://www.guru99.com/');
+    cy.wait(5000);
+    cy.xpath("//a[@title='Software Testing']").click();
+  });
+});
