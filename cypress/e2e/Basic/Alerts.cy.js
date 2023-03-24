c9b814c0649d4f2b046a7df2a43f7b6808dabf84
Tanmoy Datta <tonmoydatta45@gmail.com>
2023-03-24 18:11:26 +0600

    dropdown in cypress

--- cypress/e2e/Basic/Alerts.cy.js
+++ cypress/e2e/Basic/Alerts.cy.js
@@ -6,12 +6,25 @@ describe('Alert Test', () => {
   let js_Alert = "//button[normalize-space()='Click for JS Alert']";
   let js_Comfrim = "//button[normalize-space()='Click for JS Confirm']";
   let js_prompt = "//button[normalize-space()='Click for JS Prompt']";
-  let Alert_massage = "//p[@id='result']";
+  let massage = "//p[@id='result']";
   it('Test', () => {
     cy.wait(2000);
     cy.xpath(js_Alert).click();
+    cy.xpath(massage).should('have.text', 'You successfully clicked an alert');
     cy.wait(2000);
+
+    cy.on('window:confirm', () => {
+      return true;
+    });
     cy.xpath(js_Comfrim).click();
-    cy.xpath(Alert_massage).should('have.test', 'You clicked: Ok');
+    cy.xpath(massage).should('have.text', 'You clicked: Ok');
+    cy.wait(2000);
+
+    cy.window().then(function ($win) {
+      cy.stub($win, 'prompt').returns('Hello world');
+      cy.xpath(js_prompt).click();
+      cy.wait(1000);
+      cy.xpath(massage).should('have.text', 'You entered: Hello world');
+    });
   });
 });
---
+++ cypress/e2e/Basic/DropDown.cy.js
@@ -0,0 +1,27 @@
+describe('Open URL', () => {
+  beforeEach(() => {
+    //cy.viewport(1080,720)
+    cy.visit(
+      'https://register.rediff.com/register/register.php?FormName=user_details'
+    );
+  });
+  let day = '//tbody/tr[22]/td[3]/select[1]';
+  //let country = "//select[@id='country']";
+  it('Dropdown List', () => {
+    cy.get('[width="200"] > input').type('Test User');
+    cy.get('[valign="bottom"] > [type="text"]').type('0123456');
+    cy.get('#newpasswd').type('password');
+    cy.wait(2000);
+    cy.get('#eyeiconNew').click();
+    cy.get('#newpasswd1').type('Password1234');
+    cy.get(':nth-child(1) > [width="185"] > input').type('acd123@gmail,com');
+    cy.get('#mobno').type('01789456123');
+    //cy.get('[name="DOB_Year3360aa5a"]').select(2);
+    // cy.xpath(day).select(5).click();
+    //cy.get('[name="DOB_Month220f9000"]').select('3');
+    //cy.get('[name="DOB_Day13c4a5fb"]').select(2);
+    // cy.xpath(country).select(17);
+    cy.get('#country').select(15);
+    // cy.screenshot('Test');
+  });
+});
